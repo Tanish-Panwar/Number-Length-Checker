a5f9c13f7e7f9c973d5a177964faa79c5a046281
TANISH-PANWAR-RAJPUT <83816536+Tanish-Panwar@users.noreply.github.com>
2021-11-07 10:52:14 +0530

    Add files via upload

---
+++ Num_Length_Checker.java
@@ -0,0 +1,8 @@
+public class Num_Length_Checker {
+    public static void main(String[] args) {
+        int num = 1234567;
+        int length = (int) Math.log10(num)+1;
+        System.out.println(length);
+
+    }
+}
